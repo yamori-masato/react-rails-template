11429859497b37031a85e918758c6f3c0adda18b
yamori-masato <yamorimasato@gmail.com>
2021-03-12 00:15:34 +0900

    Appコンポーネントを修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom'
+import { logout } from './apis/auth'
+import AuthProvider from './auth/AuthProvider'
+import PrivateRoute from './auth/PrivateRoute'
+import {
+  Login,
+  Home,
+  Dashboard
+} from './pages'
 
 function App() {
   return (
-    <div className="App">
-    </div>
-  );
+    <Router>
+      <AuthProvider>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/" component={Home} />
+          <PrivateRoute exact path="/dashboard" component={Dashboard} />
+        </Switch>
+      </AuthProvider>
+    </Router>
+  )
 }
 
 export default App;
