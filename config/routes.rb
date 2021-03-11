60d87752c32a109b5fc35ab3844561968b178b75
yamori-masato <yamorimasato@gmail.com>
2021-03-11 19:51:29 +0900

    テスト用のエンドポイントを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   post 'login/', to: 'auth#create'
   delete 'logout/', to: 'auth#destroy'
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index]
+    end
+  end
 end
