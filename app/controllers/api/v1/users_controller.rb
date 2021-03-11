5b9dbd6112441e0443e72136def2ab23b3e349e8
yamori-masato <yamorimasato@gmail.com>
2021-03-12 00:16:25 +0900

    ルーティングを追加

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::UsersController < ApplicationController
     @users = User.all
     render json: @users
   end
+
+  def show
+    @user = current_user
+    render json: @user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   post 'login/', to: 'auth#create'
   delete 'logout/', to: 'auth#destroy'
+  get '/', to: 'api/v1/users#show'
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index]
